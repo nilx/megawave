138cd463acfe97e85a12f3fe805d0a0557dc4ecf
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2008-10-09 18:34:52 +0200

    scripts: relocated from the modules folder to the scripts folder

--- scripts/mw-mkmovie.sh
+++ scripts/mw-mkmovie.sh
@@ -1,88 +1,54 @@
 #!/bin/sh 
+#--------------------------- Shell MegaWave2 Macro --------------------------#
+_sccsid="%Z%MegaWave2 %R%, %M% %I%, %G%";
+_Prog="Mkmovie"
+_Vers="1.2"
+_Date="2002"
+_Func="Build a movie from an image sequence"
+_Auth="Lionel Moisan";
+_Usage="type prefix n1 n2     (type=Cmovie/Fmovie/Ccmovie/Cfmovie)"
+#----------------------------------------------------------------------------#
+# This file is part of the MegaWave2 system macros. 
+# MegaWave2 is a "soft-publication" for the scientific community. It has
+# been developed for research purposes and it comes without any warranty.
+# The last version is available at http://www.cmla.ens-cachan.fr/Cmla/Megawave
+# CMLA, Ecole Normale Superieure de Cachan, 61 av. du President Wilson,
+#       94235 Cachan cedex, France. Email: megawave@cmla.ens-cachan.fr 
+#-----------------------------------------------------------------------------
 
-# settings
-BASEDIR=/home/nil/dev/mw/code/megawave.git
-. $BASEDIR/scripts/mw-common.sh
-
-NAME=mw-mkmovie
-VERSION=1.2
-AUTHOR="Lionel Moisan <lionel.moisan@math-info.univ-paris5.fr>"
-
-USAGE="\n
-Build a movie from an image sequence.\n
-\n
-Usage: ${NAME} type prefix n1 n2\n
-\n
-n1 and n2 must verufy n2 > n1 and n2 - n1 < 10000.\n
-type must be Cmovie, Fmovie, Ccmovie or Cfmovie.\n
-\n
-Author: ${AUTHOR}\n
-"
-
-mkmovie() {
-    echo > $DEST <<EOF
-%
-MegaWave2 - DATA ASCII file -
-%
-def $DEF
-
-nimage:$N
-
-EOF
-
-    for N in `seq $N1 $N2`; do
-	echo $PREFIX'_'$N >> $DEST
-    done
-}
-
-#
-# main call
-#
-
-# no option
-if [ $# -lt 1 ]; then
-    echo "$0: an option or command is required"
-    error
+# Usage
+if [ $# -ne 4 ]; then
+  . .mw2_help_lg_com
 fi
 
-# wrapper options
-case $1 in
-    -V|--version)
-	version
-	exit
-	;;
-    -h|--help)
-	usage
-	exit
-	;;
-    -l|--list-commands)
-	list_commands
-	exit
-	;;
-esac
+def=$1
+dest=$2
+prefix=`basename $dest`
+n1=$3
+n2=$4
+n=`expr $n2 - $n1 + 1`
 
-# not enough parameters
-if [ $# -lt 3 ]; then
-    echo "$0: more parameters are required"
-    error
+if [ $n -le 0 ] || [ $n -gt 100000 ]; then
+  . .mw2_help_lg_com
+fi
+if [ $def != Cmovie ] && [ $def != Fmovie ] && [ $def != Ccmovie ] && [ $def != Cfmovie ]; then
+  . .mw2_help_lg_com
 fi
 
-TYPE=$1
-DEST=$2
-PREFIX=`basename $DEST`
-N1=$3
-N2=$4
+\rm -f $dest
+echo "%" > $dest
+echo "MegaWave2 - DATA ASCII file -" >> $dest
+echo "%" >> $dest
+echo "def $def" >> $dest
+echo "" >> $dest
+echo "nimage:$n" >> $dest
+echo "" >> $dest
 
-N=$(( $N2 - $N1 + 1))
-if [ $N -le 0 ] || [ $N -gt 100000 ]; then
-    echo "$0: bad interval"
-    error
-fi
+n=$n1
+while [ $n -le $n2 ]
+do
+    echo $prefix'_'$n >> $dest
+    n=`expr $n + 1`
+done
 
-if [ $TYPE != Cmovie ] && [ $TYPE != Fmovie ] \
-    && [ $TYPE != Ccmovie ] && [ $TYPE != Cfmovie ]; then
-    echo "$0: bad type"
-    error
-fi
-
-mkmovie
+exit 0
