8e5dfff189434474473018333d77d40bf8c854c7
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2009-03-09 17:13:42 +0100

    modules/cmla_reconstruct: remove redundant declaration
    Nested extern variables are misleading; removing the redundant
    declaration solved 6 out of 7 errors in checkmodules.sh.

--- modules/src/image/level_lines/cml_reconstruct.c
+++ modules/src/image/level_lines/cml_reconstruct.c
@@ -245,8 +245,7 @@ fill_lline(unsigned char **V, unsigned char *H1, Color level, unsigned char insi
 Cfimage
 cml_reconstruct(char *v_flag, Cmimage m_image)
 { 
-  unsigned char **V,*H1,*cptr,inc_val,inside,
-      zero_inside(struct closest_lline *);
+  unsigned char **V,*H1,*cptr,inc_val,inside;
   int NC,NL,i;
   Color current_level;
   Cfimage image_out=NULL;
